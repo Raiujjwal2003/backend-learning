8ba15cf594390baa2dfc1381c17cdc98dd81b685
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-01-02 18:41:16 +0530

    new commit

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import  express  from 'express';
+const app= express();
+
+app.get('/',(req, res)=>{
+    res.send('server is ready');
+})
+// get a list of 5 jokes
+
+app.get('/jokes',(req,res)=>{
+    const jokes=[
+
+        {
+           id:1,
+           title:'A joke',
+           contsnt:'This  is joke' 
+        },
+        {
+           id:2,
+           title:'A joke',
+           contsnt:'This  is joke' 
+        },
+        {
+           id:3,
+           title:'A joke',
+           contsnt:'This  is joke' 
+        },
+        {
+           id:4,
+           title:'A joke',
+           contsnt:'This  is joke' 
+        },
+        {
+           id:5,
+           title:'A joke',
+           contsnt:'This  is joke' 
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port ,()=>{
+    console.log(`server start at http://localhost:${port}`)
+});   
