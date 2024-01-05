87d271f9c661b3c221081dd8d36c486f2f0c1e6d
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-01-06 00:16:44 +0530

    create hospital managemant  model

---
+++ schema_learn/Models/Hospital-Managemant/hospital.models.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const hospitalSchema= new mongoose.Schema(
+  {
+
+  }, {timestamps:true}
+  
+)
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema)
---
+++ schema_learn/Models/Hospital-Managemant/patient.models.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const patientSchema= new mongoose.Schema(
+  {
+
+  }, {timestamps:true}
+  
+)
+
+export const Patient = mongoose.model("Patient", patientSchema)
