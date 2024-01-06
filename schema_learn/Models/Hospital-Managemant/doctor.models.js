76ecab974df9e1aec1b61d7ec8f8965d57ea2524
Raiujjwal2003 <raiujjwal2003@gmail.com>
2024-01-06 15:01:11 +0530

    create ecommerce model

--- schema_learn/Models/Hospital-Managemant/doctor.models.js
+++ schema_learn/Models/Hospital-Managemant/doctor.models.js
@@ -1,7 +1,31 @@
 import mongoose from 'mongoose';
 
+
+
 const doctorSchema= new mongoose.Schema(
   {
+    name:{
+      type:String,
+      required:true,
+    },
+    salary:{
+      type:String,
+      required:true,
+    },
+    qualification:{
+      type:String,
+      required:true,
+    },
+    exprerienceInYear:{
+      type:Number,
+      default:0,
+    },
+    workInHospital:[
+      {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hospital",
+      },
+    ]
 
   }, {timestamps:true}
   
--- schema_learn/Models/Hospital-Managemant/hospital.models.js
+++ schema_learn/Models/Hospital-Managemant/hospital.models.js
@@ -2,7 +2,31 @@ import mongoose from 'mongoose';
 
 const hospitalSchema= new mongoose.Schema(
   {
+    name:{
+      typr:String,
+      required:true,
+    },
+    addressLine1:{
+      type:String,
+      requried:true,
+    },
+    addressLine2:{
+      type:String,
+    },
+    city:{
+      type:String,
+      required:true,
+    },
+    pincode:{
+      type:String,
+      required:true,
 
+    },
+    spacilizedIn:[
+      {
+        type:String
+      },
+    ],
   }, {timestamps:true}
   
 )
